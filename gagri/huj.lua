3b805474581987fc77c3b9e4c31844cfa83647e9
ZareMate <najama.mg@gmail.com>
2022-06-03 19:02:23 +0200

    .

---
+++ gagri/huj.lua
@@ -0,0 +1,38 @@
+function huj(x)
+    if x == 2137 then
+        print(x)
+    else
+        print("cipa")
+    end
+    term.clear()
+    os.shutdown()
+    os.run("print ("cipa chuj")")
+    nigga(x)
+end
+
+function nigga()
+    print("nic")
+end
+huj(2137)
+
+if true then
+    dfimsdiasasim\
+elseif 2137 = huj then
+    dsdsadasd
+else
+    sdasdasdasdasd
+end
+
+while true then
+    print("nigger")
+end
+
+x = read()
+x = toNumber(x)
+sleep(x)
+
+print(x)
+return(x)
+error(x)
+
+dsadasdasdadasda
